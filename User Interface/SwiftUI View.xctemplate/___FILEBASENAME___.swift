d5995071c14cc57e4c9dd2d954acc72858ccffd7
Ilias Karim <ilias.karim@gmail.com>
2024-03-04 12:49:05 -0500

    add file templates

---
+++ User Interface/SwiftUI View.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ___FILEBASENAMEASIDENTIFIER___: View {
+  var body: some View {
+    VStack {
+      Text("Hello, world!")
+    }.padding()
+  }
+}
+
+#Preview {
+  ___FILEBASENAMEASIDENTIFIER___()
+}
